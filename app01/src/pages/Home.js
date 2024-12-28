098f6558a2554817dad568c766ca59cc0c433cc1
Sunith-P-Pathirana <sunithkaushalya.pp@gmail.com>
2024-12-28 09:09:04 +0530

    Update Home.js

--- app01/src/pages/Home.js
+++ app01/src/pages/Home.js
@@ -5,13 +5,15 @@ export default function Home() {
   return (
     <div>
       <h1>Home Page</h1>
-      <TopicBox food="Kottu" price="Rs 200">
+      <TopicBox food="Kottu" price="Rs 200"><br></br>
         <span>this is the description</span>
       </TopicBox>
-      <TopicBox food="Rice" price="Rs 1200">
+      <TopicBox food="Rice" price="Rs 1200"><br></br>
         <button>This is a button</button>
       </TopicBox>
-      <TopicBox food="Noodless" price="Rs 2200"/>
+      <TopicBox food="Noodless" price="Rs 2200"><br></br>
+        <p>wertk bckcjd ylkjea hc,mljeksdzk jhcfnem nlka fg </p>
+      </TopicBox>
     </div>
   )
 }
