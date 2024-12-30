adec6a53cd4c0d92572a3777f2a9e35419393a55
Sunith-P-Pathirana <sunithkaushalya.pp@gmail.com>
2024-12-30 11:54:52 +0530

    edit

--- app01/src/App.js
+++ app01/src/App.js
@@ -1,20 +1,38 @@
+import React from 'react';
 import './App.css';
-import Home from './pages/Home';
-import AAbout from './pages/AAbout';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Contact from './pages/Contact';
+import Accordion from 'react-bootstrap/Accordion';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/AAbout" element={<AAbout/>} />
-          <Route path="/Contact" element={<Contact />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <h1>Bootstrap</h1>
+      <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+    </div>
   );
 }
 
