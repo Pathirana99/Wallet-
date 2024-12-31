aeedb5013e2c2a78f2bd363b0bd48fe24ad0ff6f
Sunith-P-Pathirana <sunithkaushalya.pp@gmail.com>
2024-12-31 13:30:03 +0530

    Update App.js

--- app01/src/App.js
+++ app01/src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
-function buttonClick(){
-  console.log("Button is clicked.")
-  alert("Button is Clicked")
-}
 function App() {
   return (
     <div>
-      <h1>React event handling...</h1>
-      <br></br><hr></hr>
-      <button onClick={buttonClick}>Click Here</button>
+      <Button>
+        aaa
+      </Button>
     </div>
   );
 }
