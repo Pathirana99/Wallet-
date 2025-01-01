06bf057991305ac8124a5ee97960882f100ce33e
Sunith-P-Pathirana <sunithkaushalya.pp@gmail.com>
2025-01-01 12:02:57 +0530

    remove

--- app01/src/App.js
+++ app01/src/App.js
@@ -1,27 +1,11 @@
-import React, { useState, useEffect } from 'react';
 import './App.css';
 
+
 function App() {
-const [count, setCount] = useState(0);
-
-const increment = ()=>{
-  setCount(count + 1);
-};
-
-const decrement = ()=>{
-  setCount(count - 1);
-};
-
-useEffect(()=>{
-  console.log(`New count is: ${count}`); // Correct
-},[]
-);
 
   return (
     <div>
-      <h1>Count: {count}</h1>
-      <button onClick={increment}>+</button>
-      <button onClick={decrement}>-</button>
+      
     </div>
   );
 }
