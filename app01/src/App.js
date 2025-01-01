a0eff2aa642c6412f7935c1b6eb38626ef1fdbe3
Sunith-P-Pathirana <sunithkaushalya.pp@gmail.com>
2025-01-01 12:28:49 +0530

    home

--- app01/src/App.js
+++ app01/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
-
+import React from 'react';
+import Home from './Home/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- app01/src/Home/Home.js
+++ app01/src/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import './home.css'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home page</h1>
+    <div className="home">
+        <h1>WALLET</h1>
+        <h2>Save your money and save your future</h2>
     </div>
   )
 }
