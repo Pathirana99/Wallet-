431ec60a335c45650d733852daf7675360b02ac7
Sunith-P-Pathirana <sunithkaushalya.pp@gmail.com>
2025-01-03 16:09:28 +0530

    remove

--- backend/wallet/src/main/java/com/example/wallet/repo/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.wallet.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.User;
-
-public interface UserRepo extends JpaRepository<User, Integer> {
-}
--- backend/wallet/src/main/java/com/example/wallet/service/UserService.java
+++
@@ -1,4 +0,0 @@
-package com.example.wallet.service;
-
-public class UserService {
-}
