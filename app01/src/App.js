482cb3be767995dd275b9a6c0c3ecba7cc8231de
Sunith-P-Pathirana <sunithkaushalya.pp@gmail.com>
2025-01-03 21:22:27 +0530

    Update App.js

--- app01/src/App.js
+++ app01/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import Login from './Pages/Login';
 import SignIn from './Pages/SignIn';
+import Home from './Home/Home'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavigationBar from './NavBar/NavigationBar';
 
@@ -13,7 +14,7 @@ function App() {
       <div>
         <Routes>
           <Route path='/' element={<Login/>} />
-          
+          <Route path='/Home' element={<Home/>} />
           <Route path='/SignIn' element={<SignIn/>} />
         </Routes>
       </div>
