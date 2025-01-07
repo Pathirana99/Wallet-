34e8bfd46b8d971bb3a6c97b83d76eb87c8d719b
Sunith-P-Pathirana <sunithkaushalya.pp@gmail.com>
2025-01-07 11:53:08 +0530

    add

---
+++ backend/wallet/src/main/java/com/example/wallet/Controller/LoginController.java
@@ -0,0 +1,12 @@
+package com.example.wallet.Controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+@CrossOrigin(origins = "http://localhost:3000")
+public class LoginController {
+
+}
---
+++ backend/wallet/src/main/java/com/example/wallet/Dto/LoginDto.java
@@ -0,0 +1,13 @@
+package com.example.wallet.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginDto {
+    private String email;
+    private String password;
+}
