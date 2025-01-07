1fae0e30353ed66b9153ff0395271b7198f8e18a
Sunith-P-Pathirana <sunithkaushalya.pp@gmail.com>
2025-01-07 12:24:05 +0530

    edit home

--- app01/src/Home/Home.js
+++ app01/src/Home/Home.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import './home.css'
+import React from 'react';
+import './home.css';
 
 export default function Home() {
   return (
     <div className="home">
-        <div className="sidebar">
-          <div className="profilePic">
-            <div className="profileLetter">S</div>
-          </div>
-          <div className="userName">Sunith P Pathirana</div>
-          <button className="logOut">Log Out</button>
+      <div className="sidebar">
+        <div className="profilePic">
+          <div className="profileLetter">S</div>
         </div>
-        <div className="mainContent">
+        <div className="userName">Sunith P Pathirana</div>
+        <button className="logOut">Log Out</button>
+      </div>
+      <div className="mainContent">
         <div className="header">
           <h2>Sunith P Pathirana</h2>
         </div>
@@ -25,11 +25,11 @@ export default function Home() {
             <input type="text" placeholder="Enter amount" />
           </div>
           <div className="balance">
-          <div className="balance">balance</div>
+            <div className="balanceValue">Balance</div>
           </div>
-          <button className="add">ADD</button>
+          <button className="addButton">ADD</button>
         </div>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
